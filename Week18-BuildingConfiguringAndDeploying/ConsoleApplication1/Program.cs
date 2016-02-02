e708c619956890972f3305ce158a0f8751038728
SallyBoulton <yellowbox@sky.com>
2016-02-02 12:48:08 +0000

    try again

---
+++ Week18-BuildingConfiguringAndDeploying/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console=Colorful.Console;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Debug.WriteLine("Write something to the output window...");
+            Console.WriteLine("Program has finished.  Press any key to quit.", Color.Aquamarine);
+            Console.ReadKey();
+        }
+    }
+}
